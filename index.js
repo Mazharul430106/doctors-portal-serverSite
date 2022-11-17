8c71661f6c9efd96b92e2f06925dee7403631b3b
Mazharul Islam <mazharul430@gmail.com>
2022-11-17 02:29:49 -0800

    added users data

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const run = async () => {
   try {
     const appointmentOptionsCollection = client.db('doctorsPortal').collection('appointmentOptions');
     const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+    const usersCollection = client.db('doctorsPortal').collection('users');
+
 
     app.get('/appointmentOptions', async (req, res) => {
       const date = req.query.date;
@@ -39,12 +41,45 @@ const run = async () => {
       res.send(options);
     })
 
+
+    app.get('/bookings', async (req, res)=>{
+      const email = req.query.email;
+      const query = {
+        email: email
+      }
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings)
+    })
+
+
     app.post('/bookings', async (req, res) => {
       const booking = req.body;
       // console.log(booking);
+      const query = {
+        appointmentDate: booking.appointmentDate,
+        treatment: booking.treatment,
+        email: booking.email
+      }
+      const alreadyBooking = await bookingsCollection.find(query).toArray();
+      if(alreadyBooking.length){
+        const message = `You already have a booking on ${booking.appointmentDate}`
+        return res.send({acknowledged: false, message})
+      }
+    
       const result = await bookingsCollection.insertOne(booking)
       res.send(result);
     })
+
+    app.post('/users', async(req, res)=> {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+
+
+
 
 
 
